626e56acdde8cbb3c6fb0db2a6642ab85bdbe4e2
Kevin Chen <k39chen@gmail.com>
2014-08-24 20:35:53 -0400

    Created basic appearance for social subpage

--- client/js/subpages/social.js
+++ client/js/subpages/social.js
@@ -14,9 +14,51 @@ Template.socialSubpage.events({
 	"click .addfriends-btn": function(e){
 		Router.go("/social");
 	},
+	"mouseover .friend": function(e) {
+		var el = $(e.currentTarget);
+		el.addClass("hover");
+	},
+	"mouseout .friend": function(e) {
+		var el = $(e.currentTarget);
+		el.removeClass("hover");
+	},
+	"mouseover .name": function(e) {
+		var el = $(e.target);
+		el.addClass("hover");
+	},
+	"mouseout .name": function(e) {
+		var el = $(e.target);
+		el.removeClass("hover");
+	},
+	"click .name": function(e) {
+		var el = $(e.target).parent().parent(),
+			friendId = el.attr("data-friendId");
+		Router.go("/profile/"+friendId);
+	}
 });
 Template.socialSubpage.helpers({
 	hasFriends: function() {
 		return hasFriends();
+	},
+	getFriends: function() {
+		return getFriends();
+	},
+	portrait: function(item) {
+		var self = this,
+			fb = getFacebookUserData(self);
+		return fb ? getUserPortrait(fb.id) : "";
+	},
+	name: function(item) {
+		var self = this,
+			fb = getFacebookUserData(self);
+		return fb ? fb.name : "";
+	},
+	email: function() {
+		var self = this,
+			fb = getFacebookUserData(self);
+		return fb ? fb.email : "";
+	},
+	friendId: function() {
+		return this._id;
 	}
 });
