719161ec31313b3130c23b8af29716a2550dd208
Kevin Chen <kevin.chen@veeva.com>
2014-10-22 15:39:50 -0400

    Genercized user data publications

--- server/publications.js
+++ server/publications.js
@@ -2,10 +2,12 @@
  * The current user login data.
  *
  * @publication userData
+ * @param userId {String} The user ID. Defaults to currently logged in user.
  */
-Meteor.publish("userData", function(){
+Meteor.publish("userData", function(userId){
+	userId = userId !== undefined ? userId : this.userId;
 	return Meteor.users.find(
-		{_id: this.userId},
+		{_id: userId},
 		{fields: {services: 1}}
 	);
 });
@@ -13,17 +15,20 @@ Meteor.publish("userData", function(){
  * Returns the list of current user subscriptions.
  *
  * @publication userSubscriptions
+ * @param userId {String} The user ID. Defaults to currently logged in user.
  */
-Meteor.publish("userSubscriptions", function(){
-	return Subscriptions.find({userId:this.userId});
+Meteor.publish("userSubscriptions", function(userId){
+	userId = userId !== undefined ? userId : this.userId;
+	return Subscriptions.find({userId:userId});
 });
 /**
  * Returns the list of current user subscriptions along with its corresponding anime data.
  *
  * @publication userAnimeSubscriptions
+ * @param userId {String} The user ID. Defaults to currently logged in user.
  */
-Meteor.publish("userAnimeSubscriptions", function(){
-	var userId = this.userId;
+Meteor.publish("userAnimeSubscriptions", function(userId){
+	userId = userId !== undefined ? userId : this.userId;
 	var subscriptions = Subscriptions.find({userId:userId}).fetch();
 	var scope = [];
 	// this is our way of doing projection... stupid minimongo
@@ -36,17 +41,20 @@ Meteor.publish("userAnimeSubscriptions", function(){
  * Returns the list of user friends (describing the relationship between the two users).
  *
  * @publication userFriends
+ * @param userId {String} The user ID. Defaults to currently logged in user.
  */
-Meteor.publish("userFriends",function(){
-	return Friends.find({userId: this.userId});
+Meteor.publish("userFriends",function(userId){
+	userId = userId !== undefined ? userId : this.userId;
+	return Friends.find({userId: userId});
 });
 /**
  * Returns the list of current user friends along with their user data.
  *
  * @publication userFriendsUserData
+ * @param userId {String} The user ID. Defaults to currently logged in user.
  */
-Meteor.publish("userFriendsUserData", function(){
-	var userId = this.userId;
+Meteor.publish("userFriendsUserData", function(userId){
+	userId = userId !== undefined ? userId : this.userId;
 	var friends = Friends.find({userId:userId}).fetch();
 	var scope = [];
 	// this is our way of doing projection... stupid minimongo
